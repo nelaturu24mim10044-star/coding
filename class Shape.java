e9bbae538f6010060edf95ddd46a18ef79a897fc
nelaturu24mim10044-star <nelaturu.24mim10044@vitbhopal.ac.in>
2025-12-01 22:17:14 +0530

    Add files via upload

---
+++ class Shape.java
@@ -0,0 +1,49 @@
+class Shape {
+    double calculateArea() {
+        return 0;
+    }
+}
+
+class Circle extends Shape {
+    double radius;
+    Circle(double r) { radius = r; }
+
+    double calculateArea() {
+        return 3.14 * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    double l, b;
+    Rectangle(double l, double b) { this.l = l; this.b = b; }
+
+    double calculateArea() {
+        return l * b;
+    }
+}
+
+class Triangle extends Shape {
+    double base, height;
+    Triangle(double b, double h) { base = b; height = h; }
+
+    double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Shape[] shapes = {
+            new Circle(5),
+            new Rectangle(4, 6),
+            new Triangle(3, 8)
+        };
+
+        String[] names = {"Circle", "Rectangle", "Triangle"};
+
+        for (int i = 0; i < shapes.length; i++) {
+            System.out.println("Area of " + names[i] + " = " + shapes[i].calculateArea());
+        }
+    }
+}
